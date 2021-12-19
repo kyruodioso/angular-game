debefdc772d1300d4d59a4c310262381a9889267
kyruodioso <kanu_nirvana@hotmail.com>
2021-12-19 14:41:13 -0300

    firt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dados';
+
+  dadoIzquierdo="../assets/img/dice1.png";
+  dadoDerecho="../assets/img/dice3.png";
+  numero1=2;
+  numero2=1;
+
+  tirarDados():void{
+    this.numero1=Math.round(Math.random() * 5) + 1;
+    this.numero2=Math.round(Math.random() * 5) + 1;
+    this.dadoDerecho= `../assets/img/dice${this.numero1}.png`
+    this.dadoIzquierdo= `../assets/img/dice${this.numero2}.png`
+  }
 }
